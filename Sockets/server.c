6427b489a80bc075e3cfa79b1eca6cd45d465121
Alexis Rodriguez <44281620+binexisHATT@users.noreply.github.com>
2020-03-08 14:36:30 -0400

    Update server.c

--- Sockets/server.c
+++ Sockets/server.c
@@ -6,34 +6,37 @@
 
 int main () {
     
+    //**| message to send over socket
+    
     char msg[50] = "Successfully connected to server";
     
-    //**|creating socket|
+    //**| creating socket
     
     int serv_soc;
-    serv_soc = socket(AF_INET, SOCK_STREAM, 0);
+    serv_soc = socket( AF_INET, SOCK_STREAM, 0 );
     
-    //**|defining sockets address|
+    //**| defining sockets address
     
     struct sockaddr_in serv_addr;
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(1234);
+    serv_addr.sin_port = htons( 1234 );
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     
-    //**|bind our socket| 
+    //**| bind our socket
     
-    bind(serv_soc, (struct* sockaddr) &serv_addr, sizeof(serv_addr));
+    bind( serv_soc, (struct* sockaddr) &serv_addr, sizeof(serv_addr) );
     
-    //**|listen for maximum 5 incomming connections|
+    //**| listen for maximum 5 incomming connections|
     
-    listen(serv_soc, 5);
+    listen( serv_soc, 5 );
     
-    //**|accept incoming connections|
+    //**| accept incoming connections
     int conn;
-    conn = accept(serv_soc, NULL, NULL);
+    conn = accept( serv_soc, NULL, NULL );
     
-    //**|send a message to client|
-    send(conn, msg, sizeof(msg), 0);
+    //**| send a message to client
+    
+    send( conn, msg, sizeof(msg), 0 );
     
   return 0;
 }
