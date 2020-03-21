0f3aa8470c9d135201bbce8a943502a7b5bd6398
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-03-20 21:31:14 -0400

    updating repo

--- Tic-Tac-Toe/tic-tac-toe.c
+++ Tic-Tac-Toe/tic-tac-toe.c
@@ -62,6 +62,12 @@ int main() {
 }
 
 
+/*
+ * Function: showGrid
+ * -----------------------
+ * Displays welcome message
+ *
+*/
 status showGrid(int position, bool player) {
 	static char marker;
 	status v = CONTINUE;
@@ -76,6 +82,43 @@ status showGrid(int position, bool player) {
 		marker = CHOICE_X;
 	else
 		marker = CHOICE_O;
+
+	// checking value of position
+	// and changing value in 2-d grid
+	// to X or O
+	switch(position) {
+		case 1:
+			grid[0][0] = marker;
+			break;
+		case 2:
+			grid[0][1] = marker;
+			break;
+		case 3:
+			grid[0][2] = marker;
+			break;
+		case 4:
+			grid[1][0] = marker;
+			break;
+		case 5:
+			grid[1][1] = marker;
+			break;
+		case 6:
+			grid[1][2] = marker;
+			break;
+		case 7:
+			grid[2][0] = marker;
+			break;
+		case 8:
+			grid[2][1] = marker;
+			break;
+		case 9:
+			grid[2][2] = marker;
+			break;
+
+		default:
+			printf("Invalid position!");
+			exit(EXIT_SUCCESS);
+	}
 
 	/*                   THE GRID                     */
 	printf(YELLOW "\n\n\n\t\t  %c  " RESET, grid[0][0]);
@@ -102,12 +145,24 @@ status showGrid(int position, bool player) {
 }
 
 
+/*
+ * Function: startGame
+ * -----------------------
+ * Displays welcome message
+ *
+*/
 void startGame() {
 	system("clear");
 	printf(BLUE "\t       Welcome to tic tac toe!\n\n" RESET);
 }
 
 
+/*
+ * Function: promptUser
+ * -----------------------
+ * Displays welcome message
+ *
+*/
 int promptUser() {
 	int selection = 0;
 	printf("Choose a number from 1-9 (0 to exit program):> ");
@@ -119,10 +174,22 @@ int promptUser() {
 	return selection;
 }
 
-
+/*
+ * Function: checkStatus
+ * -----------------------
+ * Displays welcome message
+ *
+*/
 // status checkStatus(char gridValue[][]) {
 // }
 
+
+/*
+ * Function: endGame
+ * -----------------------
+ * Displays welcome message
+ *
+*/
 char endGame(status check, bool player) {
 	char playAgain;
 	
