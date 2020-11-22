2e758de285e7c882cf47415ba91174676074e21f
Alexis Rodriguez <rodriguez10011999@gmail.com>
2020-11-22 01:39:12 -0500

    Adding stuff to repo

--- IntroTox86/IntroX86_all_materials/IntroToAsm_code_for_class/Example2.999repeating/Example2.999repeating.c
+++
@@ -1,9 +0,0 @@
-//Goto example
-#include <stdio.h>
-int main(){
-	goto mylabel;
-	printf("skipped\n");
-mylabel:
-	printf("goto ftw!\n");
-	return 0xf00d;
-}
--- IntroTox86/IntroX86_all_materials/IntroToAsm_code_for_class/scratchpad/scratchpad.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void ExecuteSuperFunGame(){
-	printf("We both find toying with humans fun, right?\n");
-}
-
-int main(int argc, char ** argv){
-
-}
